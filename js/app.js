14fcdb472dd23d0dc62186b56139d905bab15b53
Anthony <user@Users-MBP.attlocal.net>
2019-07-07 13:31:05 -0700

    Simple Tamagotchi Game Completed

--- js/app.js
+++ js/app.js
@@ -9,10 +9,12 @@ const btnSubmit = document.getElementById('btn')
 const btnFeed = document.getElementById('btnFeed')
 const btnEnergyBoost = document.getElementById('btnEnergyBoost')
 const btnPlay = document.getElementById('btnPlay')
+const newtamgotchiObject = document.getElementsByClassName('tamagotchi')
 
 
 
 
+//Class of Tamagotchi, creates new player and increases counters
 class Tamagotchi {
     constructor() {
         this.name =  '',
@@ -20,34 +22,40 @@ class Tamagotchi {
         this.hunger = 0,
         this.sleep = 0,
         this.boredom = 0
-    }
+    } 
+  
     addHunger() {
         this.hunger++
         newHunger.textContent = 'Hunger: ' + game.player.hunger
-    
         if(game.player.hunger === 10){
-            clearInterval(game.intervals.hunger)    
+            document.querySelector('.tamagotchi').src ='img/tamagotchiDead.gif';
+            game.clearIntervals()    
+            alert(`You Forgot To Feed ${tomName.value}!`)
         } 
     }
     addSleep() {
         this.sleep++
         newSleep.textContent = 'Sleepiness: ' + game.player.sleep
         if(game.player.sleep === 10){
-            clearInterval(game.intervals.sleep)
+            document.querySelector('.tamagotchi').src ='img/tamagotchiDead.gif';
+            alert(`You Forgot To Give ${tomName.value} an Engery Boost!`)
+            game.clearIntervals()
         }
     }
     addBoredom() {
         this.boredom++
         newBoredom.textContent = 'Boredom: ' + game.player.boredom
         if(game.player.boredom === 10){
-            clearInterval(game.intervals.boredom)
+            document.querySelector('.tamagotchi').src ='img/tamagotchiDead.gif';
+            alert(`You Forgot to Play with ${tomName.value}!`)
+            game.clearIntervals()
         }
     }
     addAge() {
         this.age++
         newAge.textContent = 'Age: ' + game.player.age
-        if(game.player.age === 10) {
-            clearInterval(game.intervals.age)
+        if(game.player.hunger === 10 || game.player.sleep === 10 || game.player.boredom === 10) {
+        game.clearIntervals()            
         }
     }
 }
@@ -56,6 +64,7 @@ class Tamagotchi {
 
 
 
+//game initialization function and set intervals
 const game = {
     player: null,
     intervals : {
@@ -73,25 +82,27 @@ const game = {
     },
 
     increaseHunger() {
-        this.intervals.hunger = setInterval(this.player.addHunger.bind(this.player), 4000)
+        this.intervals.hunger = setInterval(this.player.addHunger.bind(this.player), 2000)
     },
     increaseSleep() {
         this.intervals.sleep = setInterval(this.player.addSleep.bind(this.player), 3000)
     },
     increaseBoredom() {
-        this.intervals.boredom = setInterval(this.player.addBoredom.bind(this.player), 6000)
+        this.intervals.boredom = setInterval(this.player.addBoredom.bind(this.player), 2000)
     },
     increaseAge() {
         this.intervals.age = setInterval(this.player.addAge.bind(this.player), 8000)
-    }
+    },
+    clearIntervals() {
+        clearInterval(this.intervals.hunger)
+        clearInterval(this.intervals.sleep)
+        clearInterval(this.intervals.boredom)
+        clearInterval(this.intervals.age)
+      }
 
-    // add method to increase this.player hunger, sleep, bore
-    
-    // use setInterval to increase. e.g this.hunger = setInterval(fx, 1000)
-    // to clear interval use clearInterval e.g. clearInterval(this.hunger)
-    // check to see how its doing with if else statements 
 }
-    
+
+//Initiate the game and update counters by pressing Let's Play Button
 btnSubmit.addEventListener('click', function(e){
     e.preventDefault()
 game.init()
@@ -99,10 +110,12 @@ game.increaseHunger()
 game.increaseSleep()
 game.increaseBoredom()
 game.increaseAge()
+
 console.log(game.player)
 })
 
-//update Tamagotchi Info
+
+//update Tamagotchi Info with Buttons
 
 btnFeed.addEventListener('click', function(e){
     console.log('button', game.intervals.hunger)
@@ -125,6 +138,8 @@ btnPlay.addEventListener('click', function(e){
     }
     newBoredom.textContent = 'Boredom: ' + game.player.boredom
 })
+
+
 
 
 
